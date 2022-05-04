aa572220c0dbca1e36d4ca9187e143c4b5b7d6dc
ed <allgoodata@outlook.com>
2022-05-04 14:32:06 +0300

    pets route modified

--- src/server.js
+++ src/server.js
@@ -5,13 +5,13 @@ const { PORT } = require('./config');
 
 const app = express();
 
-// route imports
-const { petsRoutes } = require('./routes/petsRoutes');
-
 // Global Middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
+// route imports
+const { petsRoutes } = require('./routes/petsRoutes');
 
 // routes
 app.use('/api/pets', petsRoutes);
